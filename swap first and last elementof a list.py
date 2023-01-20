9fd950aade8d3736a5b23879cd3b415163b301ca
surya <suryat6005@gmail.com>
2023-01-20 15:27:11 +0530

    i changed to method pop

--- swap first and last elementof a list.py
+++ swap first and last elementof a list.py
@@ -5,3 +5,12 @@ list[0],list[-1]=list[-1],list[0]
 
 print('after swap:',list)
 
+#another method pop
+
+first=list.pop(0)
+last=list.pop(-1)
+
+list.insert(0,last)
+list.append(first)
+
+print('swapping:',list)
